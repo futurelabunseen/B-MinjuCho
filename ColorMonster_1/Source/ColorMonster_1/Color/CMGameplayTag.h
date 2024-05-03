313f65184e57fb51a00daaa776b461b1adedaa19
MIN-JU-CHO <60171052+MIN-JU-CHO@users.noreply.github.com>
2024-05-03 20:01:10 +0900

    게임플레이 태그 None 색상 추가

--- ColorMonster_1/Source/ColorMonster_1/Color/CMGameplayTag.h
+++ ColorMonster_1/Source/ColorMonster_1/Color/CMGameplayTag.h
@@ -2,6 +2,7 @@
 #include "GameplayTagContainer.h"
 #include "GameplayTags.h"
 
+#define CM_COLOR_NONE FGameplayTag::RequestGameplayTag(FName("Color.None"))
 #define CM_COLOR_RED FGameplayTag::RequestGameplayTag(FName("Color.Red"))
 #define CM_COLOR_GREEN FGameplayTag::RequestGameplayTag(FName("Color.Green"))
 #define CM_COLOR_BLUE FGameplayTag::RequestGameplayTag(FName("Color.Blue"))
