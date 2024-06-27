297f961cfd153583c58a34294d5baaf64eab52d6
MIN-JU-CHO <60171052+MIN-JU-CHO@users.noreply.github.com>
2024-06-28 02:10:34 +0900

    Warning 해결

--- ColorMonster_1/Source/ColorMonster_1/UI/CMFPSHUD.cpp
+++ ColorMonster_1/Source/ColorMonster_1/UI/CMFPSHUD.cpp
@@ -68,7 +68,7 @@ void ACMFPSHUD::DrawHUD()
 		FVector2D Center(Canvas->ClipX * 0.5f, Canvas->ClipY * 0.5f);
 		FVector2D CrosshairDrawPosition(Center.X - (CrosshairTexture->GetSurfaceWidth() * 0.5f), Center.Y - (CrosshairTexture->GetSurfaceHeight() * 0.5f));
 
-		FCanvasTileItem TileItem(CrosshairDrawPosition, CrosshairTexture->Resource, FLinearColor::White);
+		FCanvasTileItem TileItem(CrosshairDrawPosition, CrosshairTexture->GetResource(), FLinearColor::White);
 		TileItem.BlendMode = SE_BLEND_Translucent;
 		Canvas->DrawItem(TileItem);
 	}
