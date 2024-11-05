42faf4ea0684db92c35b30003f3112206939a7d4
MIN-JU-CHO <60171052+MIN-JU-CHO@users.noreply.github.com>
2024-11-06 01:02:04 +0900

    Unreal Lecture Practice 1-10 (TArray, TSet)

---
+++ UnrealLectureProject/1-10/UnrealContainer/Source/UnrealContainer/MyGameInstance.cpp
@@ -0,0 +1,65 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyGameInstance.h"
+#include "Algo/Accumulate.h"
+
+void UMyGameInstance::Init()
+{
+	Super::Init();
+
+	const int32 ArrayNum = 10;
+	TArray<int32> Int32Array;
+
+	for (int32 ix = 1; ix <= ArrayNum; ++ix)
+	{
+		Int32Array.Add(ix);
+	}
+
+	// Remove even nums
+	Int32Array.RemoveAll(
+		[](int32 Val)
+		{
+			return Val % 2 == 0;
+		}
+	);
+
+	// Append
+	Int32Array += {2, 4, 6, 8, 10};
+
+	// copy from C Array and compare equality
+	TArray<int32> Int32ArrayCompare;
+	int32 CArray[] = { 1, 3, 5, 7, 9, 2, 4, 6, 8, 10 };
+	Int32ArrayCompare.AddUninitialized(ArrayNum);
+	FMemory::Memcpy(Int32ArrayCompare.GetData(), CArray, sizeof(int32) * ArrayNum);
+
+	ensure(Int32Array == Int32ArrayCompare);
+
+	int32 Sum = 0;
+	for (const int32& Int32Elem : Int32Array)
+	{
+		Sum += Int32Elem;
+	}
+
+	ensure(Sum == 55);
+
+	int32 SumByAlgo = Algo::Accumulate(Int32Array, 0);
+	ensure(Sum == SumByAlgo);
+
+	TSet<int32> Int32Set;
+	for (int32 ix = 1; ix <= ArrayNum; ++ix)
+	{
+		Int32Set.Add(ix);
+	}
+
+	Int32Set.Remove(2);
+	Int32Set.Remove(4);
+	Int32Set.Remove(6);
+	Int32Set.Remove(8);
+	Int32Set.Remove(10);
+	Int32Set.Add(2);
+	Int32Set.Add(4);
+	Int32Set.Add(6);
+	Int32Set.Add(8);
+	Int32Set.Add(10);
+}
